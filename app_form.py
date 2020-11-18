ec52fac9137f0bd793d408c4b89e4c9c2f0f5976
vcamilli <69778249+vcamilli@users.noreply.github.com>
2020-11-18 15:52:42 -0500

    adjusting
    trying to make quiz uploadable

---
+++ app_form.py
@@ -0,0 +1,16 @@
+def test(user):
+while True:
+        user_input = request.form['guess']
+        answer = user_input
+
+        if answer == "Hello World":
+            print ("Hello World! How Are You?")
+            break
+
+        else:
+            pass
+            break
+    
+        user_input = user_input.capitalize()
+test():
+
--- script1.py
+++ script1.py
@@ -1,14 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route ('/')
-def home(): 
-        return render_template('home.html')
 
-@app.route ('/winner/')
-def winner():
-        return render_template('winner.html')
+@app.route('/answer', methods=['POST'])
+def greet():
+
+    user_input = request.form['guess']
+    user_input = user_input.capitalize()
+
+    return render_template ("home.html", guess=user_input)
+
+@app.route('/')
+def home():
+    
+    return render_template("home.html")
+
 
 if __name__ == '__main__':
         app.run(debug=True)
